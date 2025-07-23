db09312b6940068758c52c6bf83ea00990c0ed9f
Dosh <muturijunior27@gmail.com>
2025-07-23 19:05:57 +0300

    updated admin

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -16,7 +16,8 @@ interface Product {
   unitsavailable: string;
   images: string[];
   category_id: string;
-  feature?: string; // Add feature property
+  feature?: string;
+  available: boolean;
 }
 
 const ProductList: React.FC = () => {
@@ -29,7 +30,9 @@ const ProductList: React.FC = () => {
 
   useEffect(() => {
     const fetchCategories = async () => {
-      const { data, error } = await supabase.from("category").select("id, name");
+      const { data, error } = await supabase
+        .from("category")
+        .select("id, name");
       if (error) {
         setError("Failed to fetch categories.");
         console.error(error.message);
@@ -44,7 +47,11 @@ const ProductList: React.FC = () => {
     const fetchProducts = async () => {
       setLoading(true);
       setError(null);
-      let query = supabase.from("plots").select("id, name, description, price, location, unitsavailable, images, category_id, features");
+      let query = supabase
+        .from("plots")
+        .select(
+          "id, name, description, price, location, unitsavailable, images, category_id, features, available"
+        );
       if (selectedCategory) {
         query = query.eq("category_id", selectedCategory);
       }
@@ -53,7 +60,12 @@ const ProductList: React.FC = () => {
         setError("Failed to fetch products. Please try again.");
         console.error(error.message);
       } else {
-        setProducts(data.map(product => ({ ...product, images: product.images || [] })) || []);
+        setProducts(
+          data.map((product) => ({
+            ...product,
+            images: product.images || [],
+          })) || []
+        );
       }
       setLoading(false);
     };
@@ -77,6 +89,7 @@ const ProductList: React.FC = () => {
         images: editingProduct.images,
         category_id: editingProduct.category_id,
         feature: editingProduct.feature,
+        available: editingProduct.available,
       })
       .eq("id", editingProduct.id);
 
@@ -95,12 +108,17 @@ const ProductList: React.FC = () => {
   return (
     <div className="min-h-screen overflow-x-hidden mt-20">
       <div className="w-full p-6 flex flex-col gap-4">
-        <h1 className="text-3xl font-bold text-center text-kilifigreen">Products</h1>
+        <h1 className="text-3xl font-bold text-center text-kilifigreen">
+          Products
+        </h1>
 
         {error && <p className="text-center text-red-500">{error}</p>}
 
         <div className="mb-2 flex items-center justify-center gap-8">
-          <label htmlFor="category" className="block text-lg font-bold text-kilifigreen mb-2">
+          <label
+            htmlFor="category"
+            className="block text-lg font-bold text-kilifigreen mb-2"
+          >
             Filter by Category
           </label>
           <select
@@ -135,7 +153,7 @@ const ProductList: React.FC = () => {
                 <tr className="bg-kilifigreen text-white">
                   <th className="py-3 px-4 text-left">Name</th>
                   <th className="py-3 px-4 text-left">Price</th>
-                  
+
                   <th className="py-3 px-4 text-left">Actions</th>
                 </tr>
               </thead>
@@ -162,14 +180,21 @@ const ProductList: React.FC = () => {
         {editingProduct && (
           <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center mt-20 p-8">
             <div className="bg-beige/80 p-6 rounded-lg shadow-lg w-full text-black max-h-[80vh] overflow-y-auto">
-              <h2 className="text-2xl font-bold text-center text-kilifi mb-4">Edit Product</h2>
+              <h2 className="text-2xl font-bold text-center text-kilifi mb-4">
+                Edit Product
+              </h2>
               <form>
                 <label className="block mb-2">
                   Name:
                   <input
                     type="text"
                     value={editingProduct.name}
-                    onChange={(e) => setEditingProduct({ ...editingProduct, name: e.target.value })}
+                    onChange={(e) =>
+                      setEditingProduct({
+                        ...editingProduct,
+                        name: e.target.value,
+                      })
+                    }
                     className="block w-full bg-gray-200 text-black border rounded-md px-3 py-2 mt-1"
                   />
                 </label>
@@ -177,7 +202,12 @@ const ProductList: React.FC = () => {
                   Description:
                   <textarea
                     value={editingProduct.description}
-                    onChange={(e) => setEditingProduct({ ...editingProduct, description: e.target.value })}
+                    onChange={(e) =>
+                      setEditingProduct({
+                        ...editingProduct,
+                        description: e.target.value,
+                      })
+                    }
                     className="block w-full bg-gray-200 text-black border rounded-md px-3 py-2 mt-1"
                   />
                 </label>
@@ -186,43 +216,93 @@ const ProductList: React.FC = () => {
                   <input
                     type="text"
                     value={editingProduct.price}
-                    onChange={(e) => setEditingProduct({ ...editingProduct, price: e.target.value })}
+                    onChange={(e) =>
+                      setEditingProduct({
+                        ...editingProduct,
+                        price: e.target.value,
+                      })
+                    }
                     className="block w-full bg-gray-200 text-black border rounded-md px-3 py-2 mt-1"
                   />
                 </label>
-                <label className="block mb-2">Category:
+                <label className="block mb-2">
+                  Category:
                   <select
                     value={editingProduct.category_id}
-                    onChange={(e) => setEditingProduct({ ...editingProduct, category_id: e.target.value })}
+                    onChange={(e) =>
+                      setEditingProduct({
+                        ...editingProduct,
+                        category_id: e.target.value,
+                      })
+                    }
                     className="block w-full bg-gray-200 text-black border rounded-md px-3 py-2 mt-1"
                   >
-                    {categories.map(category => (
-                      <option key={category.id} value={category.id}>{category.name}</option>
+                    {categories.map((category) => (
+                      <option key={category.id} value={category.id}>
+                        {category.name}
+                      </option>
                     ))}
                   </select>
                 </label>
-                <label className="block mb-2">Available Units:
-                  <input type="text" value={editingProduct.unitsavailable} onChange={(e) => setEditingProduct({ ...editingProduct, unitsavailable: e.target.value })} className="block w-full bg-gray-200 text-black border rounded-md px-3 py-2 mt-1" />
+                <label className="block mb-2">
+                  Available Units:
+                  <input
+                    type="text"
+                    value={editingProduct.unitsavailable}
+                    onChange={(e) =>
+                      setEditingProduct({
+                        ...editingProduct,
+                        unitsavailable: e.target.value,
+                      })
+                    }
+                    className="block w-full bg-gray-200 text-black border rounded-md px-3 py-2 mt-1"
+                  />
                 </label>
                 <label className="block mb-2">
-          Images (comma-separated URLs):
-          <input
-            type="text"
-            value={editingProduct.images.join(', ')} // Join array to string
-            onChange={(e) => setEditingProduct({ ...editingProduct, images: e.target.value.split(',').map(url => url.trim()) })} // Split string to array
-            className="block w-full bg-gray-200 text-black border rounded-md px-3 py-2 mt-1"
-          />
-        </label>
+                  Images (comma-separated URLs):
+                  <input
+                    type="text"
+                    value={editingProduct.images.join(", ")} // Join array to string
+                    onChange={(e) =>
+                      setEditingProduct({
+                        ...editingProduct,
+                        images: e.target.value
+                          .split(",")
+                          .map((url) => url.trim()),
+                      })
+                    } // Split string to array
+                    className="block w-full bg-gray-200 text-black border rounded-md px-3 py-2 mt-1"
+                  />
+                </label>
 
-        <label className="block mb-2">
-          Feature:
-          <input
-            type="text"
-            value={editingProduct.feature || ''}
-            onChange={(e) => setEditingProduct({ ...editingProduct, feature: e.target.value })}
-            className="block w-full bg-gray-200 text-black border rounded-md px-3 py-2 mt-1"
-          />
-        </label>
+                <label className="block mb-2">
+                  Feature:
+                  <input
+                    type="text"
+                    value={editingProduct.feature || ""}
+                    onChange={(e) =>
+                      setEditingProduct({
+                        ...editingProduct,
+                        feature: e.target.value,
+                      })
+                    }
+                    className="block w-full bg-gray-200 text-black border rounded-md px-3 py-2 mt-1"
+                  />
+                </label>
+                <label className="block mb-2">
+                  Available:
+                  <input
+                    type="checkbox"
+                    checked={editingProduct.available || false}
+                    onChange={(e) =>
+                      setEditingProduct({
+                        ...editingProduct,
+                        available: e.target.checked,
+                      })
+                    }
+                    className="ml-2 accent-kilifi"
+                  />
+                </label>
 
                 <div className="flex justify-between mt-4">
                   <button
